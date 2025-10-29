b0c4ee6498e7bfe082dc6a62f2ee32a221111a15
YanPontoExe <111132370+YanPontoExe@users.noreply.github.com>
2025-10-28 21:13:37 -0300

    corrigido ++ alguns imports não estavam funcionando

--- demosca/src/main/java/com/inter/demosca/Repositories/EntradaRepository.java
+++ demosca/src/main/java/com/inter/demosca/Repositories/EntradaRepository.java
@@ -3,6 +3,7 @@ package com.inter.demosca.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.inter.demosca.Entities.EntradaEntity;
 import com.inter.demosca.Entities.UsuarioEntity;
 
 @Repository
