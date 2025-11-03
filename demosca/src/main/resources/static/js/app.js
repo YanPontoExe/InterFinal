6c6db5a46c92f1c27fc15c7209da87a967baf5d5
YanPontoExe <111132370+YanPontoExe@users.noreply.github.com>
2025-11-03 17:13:52 -0300

    add access-fornecedor + modificação access material, user e index

---
+++ demosca/src/main/resources/static/js/app.js
@@ -0,0 +1,16 @@
+function entrar_materiais(){
+    window.location.href = "access-material.html";
+}
+
+function return_index(){
+    window.location.href = "index.html";
+}
+
+function entrar_usuarios(){
+    window.location.href = "access-user.html";
+}
+
+function entrar_fornecedores(){
+    window.location.href = "access-fornecedor.html";
+}
+
