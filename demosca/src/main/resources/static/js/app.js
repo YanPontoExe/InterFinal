fbd9f1b32bf412b78dbdbf762884999f3ff74fbd
YanPontoExe <111132370+YanPontoExe@users.noreply.github.com>
2025-11-04 14:25:02 -0300

    add função para cadastrar login

--- demosca/src/main/resources/static/js/app.js
+++ demosca/src/main/resources/static/js/app.js
@@ -1,3 +1,4 @@
+//funções para navegação entre páginas, pelos button's
 function entrar_materiais(){
     window.location.href = "access-material.html";
 }
@@ -14,3 +15,24 @@ function entrar_fornecedores(){
     window.location.href = "access-fornecedor.html";
 }
 
+function entrar_marcas(){
+    window.location.href = "access-marca.html";
+}
+
+//funcção para cadastrar login do funcionario
+async function cadastrar_login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    try {
+        let response = await fetch("/Usuario", {
+            method: "POST",
+            headers: {  
+                        "nome:": username,
+                        "senha:": password
+                     }
+        });
+    } catch (error) {
+        alert("Erro ao cadastrar login: " + error);
+    }
+}
