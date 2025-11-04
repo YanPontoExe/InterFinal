e2fd03171d43d27e234ffee81cc04d0269b42898
YanPontoExe <111132370+YanPontoExe@users.noreply.github.com>
2025-11-04 16:32:28 -0300

    add mais telas + update index

--- demosca/src/main/resources/static/js/app.js
+++ demosca/src/main/resources/static/js/app.js
@@ -19,20 +19,7 @@ function entrar_marcas(){
     window.location.href = "access-marca.html";
 }
 
-//funcção para cadastrar login do funcionario
-async function cadastrar_login(){
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    try {
-        let response = await fetch("/Usuario", {
-            method: "POST",
-            headers: {  
-                        "nome:": username,
-                        "senha:": password
-                     }
-        });
-    } catch (error) {
-        alert("Erro ao cadastrar login: " + error);
-    }
+function exit(){
+    window.location.href = "login.html";
 }
+
