9521877444092e4982aa1d3b2129e76cf56708d9
YanPontoExe <111132370+YanPontoExe@users.noreply.github.com>
2025-11-05 15:23:57 -0300

    consertados caminhos para os buttons

--- demosca/src/main/resources/static/js/app.js
+++ demosca/src/main/resources/static/js/app.js
@@ -1,26 +1,26 @@
 //funções para navegação entre páginas, pelos button's
 function entrar_materiais(){
-    window.location.href = "access-material.html";
+    window.location.href = "/access-material.html";
 }
 
 function return_index(){
-    window.location.href = "index.html";
+    window.location.href = "/index.html";
 }
 
 function entrar_usuarios(){
-    window.location.href = "access-user.html";
+    window.location.href = "/access-user.html";
 }
 
 function entrar_fornecedores(){
-    window.location.href = "access-fornecedor.html";
+    window.location.href = "/access-fornecedor.html";
 }
 
 function entrar_marcas(){
-    window.location.href = "access-marca.html";
+    window.location.href = "/access-marca.html";
 }
 
 function exit(){
-    window.location.href = "login.html";
+    window.location.href = "/login.html";
 }
 
 
