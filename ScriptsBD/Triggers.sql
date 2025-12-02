0f73dd1a9b6200fce9746a4d880ecb3b371b3b47
Yan Henrry <yan.zhenrry@gmail.com>
2025-12-02 19:34:47 -0300

    Trigger corrigida, DER atualizado os campos de usuario

--- ScriptsBD/Triggers.sql
+++ ScriptsBD/Triggers.sql
@@ -1,36 +1,15 @@
-create trigger tg_AttEstoqueSaida
-on tb_saida
-instead of insert
-as
-begin
-	update tb_estoque set qtd_atual = qtd_atual - (	
-		select m.quantidade
-		from tb_movimentacao as m, inserted as i, tb_saida as s
-		where @@IDENTITY = m.id_movimentacao  --pegando a ultima movimentação cadastrada
-		and i.data_saida =  s.data_saida --identificando que é uma movimentação de saida
-	)
-end
-go
+--Trigger da tabela tb_funcionario
+--atualiza automaticamente a coluna data_contratacao com a data e hora atual só quando um novo registro é inserido com sucesso
 
+CREATE TRIGGER trg_funcionario_data_contratacao
+ON tb_funcionario
+AFTER INSERT
+AS
+BEGIN
+    
+    UPDATE T
+    SET T.data_contratacao = GETDATE() 
+    FROM tb_funcionario AS T
+    INNER JOIN Inserted AS I ON T.id_funcionario = I.id_funcionario;
 
-create trigger tg_AttEstoqueEntrada
-on tb_entrada
-instead of insert
-as
-begin
-	update tb_estoque set qtd_atual = qtd_atual + (	
-		select m.quantidade
-		from tb_movimentacao as m, inserted as i, tb_entrada as e
-		where @@IDENTITY = m.id_movimentacao  --pegando a ultima movimentação cadastrada
-		and i.data_entrada =  e.data_entrada --identificando que é uma movimentação de saida
-	)
-end
-go
-
-
-
-select * from tb_entrada
-select * from tb_saida
-
-
-
+END;
